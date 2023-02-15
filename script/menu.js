867d68008bc1c65bc99442211dccc731deac6c0f
Nikita-Golubtsov <nproger781@gmail.com>
2023-02-15 19:16:30 +0700

    card_prod

--- script/menu.js
+++ script/menu.js
@@ -1,12 +1,23 @@
 const menu_adapt = document.querySelector('.adapt');
-const nav_items = document.querySelectorAll('.adapt .nav-item');
+// ссылки адаптивного меню
+const nav_items_adapt = document.querySelectorAll('.adapt .nav-item');
 const btn_adapt = document.querySelector('.btn-adapt-menu');
 btn_adapt.addEventListener('click', () => {
     if(check_active(btn_adapt)){
-        remove_menu();
+        remove_menu(btn_adapt, menu_adapt, nav_items_adapt);
     } else {
-        get_menu();
+        get_menu(btn_adapt, menu_adapt, nav_items_adapt);
     }
+});
+
+// ссылки li>a на каталог
+const nav_items_items = document.querySelectorAll('.nav-items-catalog .nav-item');
+// список ul под ссылкой каталог
+const nav_items_catalog = document.querySelector('.nav-items-catalog');
+// ссылка li#id=nav-item-catalog  каталог
+const nav_item_catalog = document.querySelector('#nav-item-catalog');
+nav_item_catalog.addEventListener('mouseover', () => {
+        get_menu(nav_item_catalog, nav_items_catalog, nav_items_items);
 });
 
 function check_active(el){
@@ -16,28 +27,28 @@ function check_active(el){
     return false;
 }
 
-function remove_menu(){
-    btn_adapt.classList.remove('active');
-    for (const el of nav_items) {
+function remove_menu(btn, elem, links){
+    btn.classList.remove('active');
+    for (const el of links) {
         el.style.opacity = 0;
     }
     setTimeout(() => {
-        menu_adapt.style.height = 0;
+        elem.style.height = 0;
     }, 200);
-    menu_adapt.style.height = 0;
+    elem.style.height = 0;
     setTimeout(() => {
-        menu_adapt.style.display = 'none';
+        elem.style.display = 'none';
     }, 600);
 }
 
-function get_menu(){
-    btn_adapt.classList.add('active');
-    menu_adapt.style.display = 'flex';
+function get_menu(btn, elem, links){
+    btn.classList.add('active');
+    elem.style.display = 'block';
     setTimeout(() => {
-        menu_adapt.style.height = menu_adapt.scrollHeight + 'px';
+        elem.style.height = elem.scrollHeight + 'px';
     }, 200);
     setTimeout(() => {
-        for (const el of nav_items) {
+        for (const el of links) {
             el.style.opacity = 1;
         }
     }, 600);
