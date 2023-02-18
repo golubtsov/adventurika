b61f4f10df7793177b128ee7878b5472ca003728
Nikita-Golubtsov <nproger781@gmail.com>
2023-02-18 17:59:02 +0700

    sql

---
+++ server/db/db.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "adventurika";
+
+$connect = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
---
+++ server/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+include("./db/db.php");
+
+if($_POST){
+    echo "POST";
+}
+
+?>
